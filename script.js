19480443a529e9d728d7f12cdc19bb7eb2ab0cc5
Sara Roseane <80297582+sararoseane@users.noreply.github.com>
2023-08-01 16:08:28 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // if (html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    //se estiver light mode, adicionar a imagem
+    img.setAttribute("src", "./assets/avatar-light.jpg")
+  } else {
+    //se tiver sem light mode, manter a imagem
+    img.setAttribute(
+      "src",
+      ".https://avatars.githubusercontent.com/u/80297582?v=4"
+    )
+  }
+}
